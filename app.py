e8495b5decf1475e118d3e71824b9c42af62eb57
tanimoyan <tanimo1111@gmail.com>
2021-10-18 01:23:54 +0900

    third

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
 import os
+import hashlib
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
@@ -26,7 +27,7 @@ login_manager = LoginManager()
 #LoinManagerと今回作成したappの紐付け
 login_manager.init_app(app)
 
-#db.Modelはsqlalchemyを使用する際に用いるクラス
+# # db.Modelはsqlalchemyを使用する際に用いるクラス
 # class Post(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     title = db.Column(db.String(50), nullable=False)
@@ -44,45 +45,20 @@ class User(UserMixin, db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route("/signup", methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        #長さが256ビットの適当な値を返す
-        user = User(username=username, password=generate_password_hash(password, method='sha256'))
-
-        #データベースに追加
-        db.session.add(user)
-        #データベースに保存->変更が保存反映
-        db.session.commit()
-        return redirect('/login')
-    else:
-        return render_template('signup.html')
-
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
 
-        #usernameでフィルターをかけてそこに合致したものを取ってくる(Userのテーブルから取ってくる)
-        user = User.query.filter_by(username=username).first() #複数取らないように始めだけ取るfirst()
-        #userのパスワード情報とフォームに入力したパスワード情報が等しいか判別する
-        if check_password_hash(user.password, password):
-            login_user(user) #ユーザー名が等しければログインし、ホームページに飛ぶ
+        if username == 'tanimoto' and hashlib.sha256(password.encode('utf-8')).hexdigest() == '819f03c3cc46f68995b15b627bec40b65a9269205543fdff966a09b9e12c3136':
+            return redirect('/height')
+        else:
             return redirect('/')
     else:
         return render_template('login.html')
 
-@app.route('/logout')
-@login_required #ログインしているユーザーしかアクセスできない(ログインしている前提)
-def logput():
-    logout_user()
-    return redirect('/login')
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/height", methods=["GET", "POST"])
 def upload_file():
     if request.method == "GET":
         return render_template("index.html")
@@ -104,9 +80,10 @@ def upload_file():
 
         pil_img.save(contour_img_path)
 
-        # # 予測を実施
-        # output = model(image)
-        # _, prediction = torch.max(output, 1)
-        # result = prediction[0].item()
-
         return render_template("index.html", filepath=filepath, result=result, contour_img_path=contour_img_path, sure_fig_p=sure_fig_p)
+
+@app.route('/height')
+@login_required #ログインしているユーザーしかアクセスできない(ログインしている前提)
+def logput():
+    logout_user()
+    return redirect('/')
